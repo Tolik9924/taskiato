718ef7029bf9ef608e89fd9e85b43b633dbedc3a
Tolik9924 <76564089+Tolik9924@users.noreply.github.com>
2021-10-18 18:03:00 +0300

    Form person info

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+                    name: '',
+                    surname: '',
+                    fathername: '',
+                    sex: '',
+                    phone: ''
+                  };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleChangeLastname = this.handleChangeLastname.bind(this);
+    this.handleChangeFathername = this.handleChangeFathername.bind(this);
+    this.onChangeValue = this.onChangeValue.bind(this);
+    this.handleChangePhone = this.handleChangePhone.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({name: event.target.value});
+  }
+
+  handleChangeLastname(event) {
+    this.setState({surname: event.target.value});
+  }
+
+  handleChangeFathername(event) {
+    this.setState({fathername: event.target.value});
+  }
+
+  onChangeValue(event) {
+    this.setState({sex: event.target.value});
+  }
+
+  handleChangePhone(event) {
+    this.setState({phone: event.target.value});
+  }
+  
+  handleSubmit(event) {
+    alert('Person info: ' + this.state.name + ' ' + this.state.surname + ' ' + this.state.fathername + ' ' + this.state.sex + ' ' + this.state.phone);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div>
+          <label>
+            Name:
+            <input type="text" value={this.state.name} pattern="[A-Za-z]{2,}" onChange={this.handleChange} />
+          </label>
+        </div>
+        <div>
+          <label>
+            Surname:
+            <input type="text" value={this.state.surname} pattern="[A-Za-z]{2,}" onChange={this.handleChangeLastname} />
+          </label>
+        </div>
+        <div>
+          <label>
+            Fathername:
+              <input type="text" value={this.state.fathername} pattern="[A-Za-z]{2,}" onChange={this.handleChangeFathername} required/>
+          </label>
+        </div>
+        <div onChange={this.onChangeValue}>
+          <input type="radio" value="Male" name="sex" className="rad" /> Male
+          <input type="radio" value="Female" name="sex" className="rad" /> Female
+        </div>
+        <div>
+        <label>
+            Phone:
+            <input type="text" value={this.state.phone} pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" onChange={this.handleChangePhone} />
+          </label>
+        </div>
+        <button type="submit" value="Submit">Submit</button>
+      </form>
+    );
+  }
 }
 
-export default App;
+export default NameForm;
