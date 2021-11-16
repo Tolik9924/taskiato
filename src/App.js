3fe8a25db26bd50c1a91790a7b6a8b567073aeef
Tolik9924 <76564089+Tolik9924@users.noreply.github.com>
2021-11-16 15:39:38 +0200

    redux form

--- src/App.js
+++ src/App.js
@@ -1,70 +1,73 @@
 import './App.css';
 import React from 'react';
-import NamePerson from './components/NamePerson';
-import SurnamePerson from './components/SurnamePerson/SurnamePerson';
-import FatherName from './components/FatherName/FatherName';
-import SexPerson from './components/SexPerson/SexPerson';
-import PhonePerson from './components/PhonePerson/PhonePerson';
+import { Field, reduxForm } from 'redux-form';
 
-class NameForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      surname: '',
-      fathername: '',
-      sex: '',
-      phone: ''
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleChangeLastname = this.handleChangeLastname.bind(this);
-    this.handleChangeFathername = this.handleChangeFathername.bind(this);
-    this.onChangeValue = this.onChangeValue.bind(this);
-    this.handleChangePhone = this.handleChangePhone.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({ name: event.target.value });
-  }
-
-  handleChangeLastname(event) {
-    this.setState({ surname: event.target.value });
-  }
-
-  handleChangeFathername(event) {
-    this.setState({ fathername: event.target.value });
-  }
-
-  onChangeValue(event) {
-    this.setState({ sex: event.target.value });
-  }
-
-  handleChangePhone(event) {
-    this.setState({ phone: event.target.value });
-  }
-
-  handleSubmit(event) {
-    alert('Person info: ' + this.state.name + ' ' + this.state.surname + ' ' + this.state.fathername + ' ' + this.state.sex + ' ' + this.state.phone);
-    event.preventDefault();
-  }
-
-  render() {
-    return (
-      <form className="register-form-container" onSubmit={this.handleSubmit}>
-        <div>
-          <p className="form-title">Інформація користувача</p>
-          <NamePerson  name={this.state.name} handleChange={this.handleChange} />
-          <SurnamePerson surname={this.state.surname} handleChangeLastname={this.handleChangeLastname } />
-          <FatherName fathername={this.state.fathername} handleChangeFathername={this.handleChangeFathername} />
-          <SexPerson onChangeValue={this.onChangeValue} />
-          <PhonePerson phone={this.state.phone} handleChangePhone={this.handleChangePhone} />
-          <button type="submit" value="Submit" className="button">Submit</button>
-        </div>
-      </form>
-    );
-  }
+const PersonForm = (props) => {
+  return(
+    <form onSubmit={props.handleSubmit}>
+      <h1 className='form-title'>Person data</h1>
+      <div className='form-field'>
+        <Field placeholder={'Name'} 
+               name={'name'}  
+               pattern={'[A-Za-z]{2,}'} 
+               component={'input'}
+               required />
+      </div>
+      <div>
+        <Field placeholder={'SecondName'} 
+              name={'secondName'} 
+              pattern={'[A-Za-z]{2,}'} 
+              component={'input'} 
+              required />
+      </div>
+      <div>
+        <Field placeholder={'FatherName'} 
+              name={'fatherName'} 
+              pattern={'[A-Za-z]{2,}'} 
+              component={'input'}  
+              required />
+      </div>
+      <div className='form-field'>
+        <p className='form-text'>Your sex: </p>
+        <Field type={'radio'} 
+               id={'malesex'} 
+               name={'sex'} 
+               value={'male'} 
+               className={'rad'} 
+               component={'input'}
+               required />
+        <Field for={'malesex'} component={'label'}  /> Male
+        <Field type={'radio'} 
+               id={'womensex'} 
+               name={'sex'} 
+               value={'women'} 
+               className={'rad'} 
+               component={'input'} />
+        <Field for={'womensex'} component={'label'} /> Female
+      </div>
+      <div>
+       <Field placeholder={'PhoneNumber'} type={'text'} id={'mob'} name={'phone'} pattern={'[0-9]{3}-[0-9]{3}-[0-9]{4}'} component={'input'} />
+      </div>
+      <div>
+        <button className='button'>Submit</button>
+      </div>
+    </form>
+  );
 }
 
-export default NameForm;
+const PersonReduxForm = reduxForm({
+  form: 'person'
+})(PersonForm)
+
+const Person = (props) => {
+  const onSubmit = (formData) => {
+    console.log(formData);
+  }
+  return(
+    <div className='register-form-container'>
+      <PersonReduxForm onSubmit={onSubmit} />
+    </div>
+  );
+}
+
+export default Person;
